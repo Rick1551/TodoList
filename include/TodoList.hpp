74bccff68adc1fbf6761cee601528b68ee73a9f1
Ricardo Saldaña <ricardo_scuentas@outlook.com>
2025-07-17 16:45:52 -0500

    Agregamos CMakeLists y base de main

--- include/TodoList.hpp
+++ include/TodoList.hpp
@@ -1,1 +1,23 @@
 #pragma once
+#include <string>
+#include <vector>
+
+enum class Prioridad {Baja, Media, Alta};
+
+struct Tarea{
+    int id;
+    std::string descripcion;
+    Prioridad prioridad;
+    bool completada;
+};
+
+class TodoList {
+    public:
+        void agregarTarea(const std::string& descripcion, Prioridad prio);
+        void listarTareas() const;
+        void marcarCompletada(int id);
+
+    private:
+        std::vector<Tarea> tareas_;
+        int siguiente_id_ = 1;
+};
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "TodoList.hpp"
+
+int main(){
+    TodoList lista;
+    int opcion;
+
+    do{
+        std::cout << "\n--- To Do List ---\n"
+                  << "1. Añadir tarea\n"
+                  << "2. Listar tareas\n"
+                  << "3. Maracar como completada\n"
+                  << "4. Salir\n"
+                  << "Opcion: ";
+        std::cin >> opcion;
+
+        //Switch
+
+    } while (opcion != 4);
+    return 0;
+}
