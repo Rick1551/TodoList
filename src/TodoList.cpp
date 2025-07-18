800993caa838738663743c1b9929fa7c924e4328
Ricardo Saldaña <ricardo_scuentas@outlook.com>
2025-07-17 23:30:18 -0500

    Implementamos agregar tarea

---
+++ src/TodoList.cpp
@@ -0,0 +1,12 @@
+#include "TodoList.hpp"
+
+Tarea tarea1{
+    1,
+    "Estudiar C++",
+    Prioridad::Alta,
+    false
+};
+
+void agregarTarea(const std::string& descripcion, Prioridad prio){
+    
+}
